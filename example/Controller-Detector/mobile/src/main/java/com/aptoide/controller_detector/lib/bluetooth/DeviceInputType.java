4e8b79e5519b4822c1389fe61682c626078ca386
franciscoaleixo <francisco.aleixo16@gmail.com>
2016-07-20 16:04:35 +0100

    Initial release of library and example project.

--- example/Controller-Detector/mobile/src/main/java/com/aptoide/controller_detector/lib/bluetooth/DeviceInputType.java
+++
@@ -1,9 +0,0 @@
-package com.aptoide.controller_detector.lib.bluetooth;
-
-/**
- * Only for peripheral devices
- * See: http://bluetooth-pentest.narod.ru/software/bluetooth_class_of_device-service_generator.html
- */
-public enum DeviceInputType {
-    UNCATEGORIZED, JOYSTICK, GAMEPAD, REMOTECONTROL, SENSINGDEVICE, DIGITIZERTABLET, CARDREADER
-}
