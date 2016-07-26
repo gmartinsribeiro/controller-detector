042dc40b71475a9f1646afcee56a02457d24bf40
Francisco Aleixo <francisco.aleixo16@gmail.com>
2016-07-26 17:45:34 +0100

    Update .travis.script.sh

--- .travis.script.sh
+++ .travis.script.sh
@@ -1,3 +1,3 @@
-echo "$BINTRAY_USER" >> local.properties
-echo "$BINTRAY_APIKEY" >> local.properties
-echo "$BINTRAY_GPG_PASSWORD" >> local.properties
+echo "bintray.user=$BINTRAY_USER" >> local.properties
+echo "bintray.apikey=$BINTRAY_APIKEY" >> local.properties
+echo "bintray.gpg.password=$BINTRAY_GPG_PASSWORD" >> local.properties
